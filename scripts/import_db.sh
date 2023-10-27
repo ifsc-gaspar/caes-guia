793d8850ae94208454673e2cf2c16604b80849d6
Renato Simões <renatosm@gmail.com>
2023-10-27 00:09:27 -0300

    Exportando password

--- scripts/import_db.sh
+++ scripts/import_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+export PGPASSWORD=postgres
 psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/script_criacao_tabelas.sql
 psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/insert_estado.sql
 psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/insert_cidade.sql
