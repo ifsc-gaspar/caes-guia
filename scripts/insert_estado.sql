e12726801fe03764150083619b1e011411426eb0
Renato Simões <renatosm@gmail.com>
2023-10-26 21:52:43 -0300

    Add file
    Adicionando scripts de banco

---
+++ scripts/insert_estado.sql
@@ -0,0 +1,30 @@
+INSERT INTO public.estado (nome,sigla) VALUES
+	 ('Acre','AC'),
+	 ('Alagoas','AL'),
+	 ('Amazonas','AM'),
+	 ('Amapá','AP'),
+	 ('Bahia','BA'),
+	 ('Ceara','CE'),
+	 ('Distrito Federal','DF'),
+	 ('Espirito Santo','ES'),
+	 ('Goiás','GO'),
+	 ('Maranhão','MA');
+INSERT INTO public.estado (nome,sigla) VALUES
+	 ('Minas Gerais','MG'),
+	 ('Mato Grosso do Sul','MS'),
+	 ('Mato Grosso','MT'),
+	 ('Pará','PA'),
+	 ('Paraíba','PB'),
+	 ('Pernambuco','PE'),
+	 ('Piauí','PI'),
+	 ('Paraná','PR'),
+	 ('Rio de Janeiro','RJ'),
+	 ('Rio Grande do Norte','RN');
+INSERT INTO public.estado (nome,sigla) VALUES
+	 ('Rondônia','RO'),
+	 ('Roraima','RR'),
+	 ('Rio Grande do Sul','RS'),
+	 ('Santa Catarina','SC'),
+	 ('Sergipe','SE'),
+	 ('São Paulo','SP'),
+	 ('Tocantins','TO');
