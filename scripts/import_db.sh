9b107584efcb79937f4c413a7844d56c3b439dd3
Renato Simões <renatosm@gmail.com>
2023-10-27 00:01:35 -0300

    Adicionando import_db.sh
    Add

---
+++ scripts/import_db.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/script_criacao_tabelas.sql
+psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/insert_estado.sql
+psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/insert_cidade.sql
+psql -h db -U postgres -d escola-caes-guia -a -f /caes-guia/scripts/insert_endereco.sql
